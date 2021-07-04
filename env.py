2ada919ffe78104bc2b40184e38586739dd9b12f
ColinHammarberg <colin.hammarberg2@gmail.com>
2021-07-04 12:30:06 +0000

    Updated MONGO_URI

--- env.py
+++ env.py
@@ -3,5 +3,5 @@ import os
 os.environ.setdefault("IP", "0.0.0.0")
 os.environ.setdefault("PORT", "5000")
 os.environ.setdefault("SECRET_KEY", "(a^UphGimLXTnZ9,Q@97Sh~A&uMZ6e")
-os.environ.setdefault("MONGO_URI", "mongodb+srv://colinhammarberg:Grym123@myFirstCluster-4g3i1.mongodb.net/task_manager?retryWrites=true&w=majority")
+os.environ.setdefault("MONGO_URI", "mongodb+srv://colinhammarberg:t9jMKuCS8nrs0155@myfirstcluster.l6iff.mongodb.net/task_manager?retryWrites=true&w=majority")
 os.environ.setdefault("MONGO_DBNAME", "task_manager")
